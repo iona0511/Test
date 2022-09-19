7a8c43dfe5206cd4ad782a35e5393f4ba10803b6
iona0511 <lilly800511@gmail.com>
2022-09-19 15:12:11 +0800

    layout practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,147 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Form from "./images/svg/form.svg";
+import Submit from "./images/svg/submit.svg";
+import CertificateA from "./images/svg/certificateA.svg";
+import CertificateD from "./images/svg/certificateD.svg";
+import Awards from './components/Awards';
+import Lighthouse from "./images/svg/lighthouse.svg";
+import Market from "./images/svg/market.svg";
+import Turtle from "./images/svg/turtle.svg";
+import Color from "./images/svg/color.svg";
+import Vector4 from "./images/svg/Vector4.svg";
+import Vector5 from "./images/svg/Vector5.svg";
+import Sup from "./images/svg/sup.svg";
+import Sup1 from "./images/svg/sup1.svg";
+import './App.scss';
+const App = () => {
+  const [formInput, setFormInput] = useState({ store: '', name: '', amount: '', phone: '', payment: "" });
+  const [open, setOpen] = useState(false);
+  const awardText = [
+    { id: 1, award: 'A', number: 'ONE', text: 'dehumidifier', certificate: CertificateA },
+    { id: 2, award: 'B', number: 'ONE', text: 'range hood', certificate: CertificateA },
+    { id: 3, award: 'C', number: 'ONE', text: 'vacuum cleaner', certificate: CertificateA },
+    { id: 4, award: 'D', number: 'ONE', text: 'toaster', certificate: CertificateD },
+    { id: 5, award: 'E', number: 'ONE', text: 'scale', certificate: CertificateD },
+    { id: 6, award: 'F', number: 'ONE', text: 'straightening iron', certificate: CertificateD },
+    { id: 7, award: 'G', number: 'FIVE', text: 'vacuum cleaner', certificate: CertificateD },
+    { id: 8, award: 'H', number: 'TEN', text: 'rice cooker', certificate: CertificateD }
+  ];
+  const stores = [{ id: 1, name: 'store1' }, { id: 2, name: '543534store2' }, { id: 3, name: '4545878678store3' }];
+  const handlePropagation = (e) => {
+    e.stopPropagation();
+    setOpen(false);
+  };
+  return (<>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className='container'>
+      <div className='background'>
+        <div className='areaFirst'>
+          <p className='text1'>There is no one</p>
+          <p className='text2'>who loves pain</p>
+
+          <img src={Form} alt='' className='Form' />
+        </div>
+        <div className='paragraphArea'>
+          <img src={Sup} alt="" className='Sup' />
+          <img src={Sup1} alt="" className='Sup1' />
+          <p className='paragraph'> paragraph</p>
+          <p>1. Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+          <p className='paragraph'>paragraph</p>
+          <p>1. Quisque sodales leo<u><b> vitae vulputate auctor.</b> </u></p>
+          <p>2. Proin ac justo ut nisl tincidunt imperdiet.Maecenas viverra libero a pellentesque blandit. Proin ac justo</p>
+          <p>3. Cras tristique tellus id leo bibendum, eu dapibus nisl accumsan.  </p>
+          <p>4. Donec ultrices sapien<b> vitae leo venenatis ullamcorper.</b></p>
+        </div>
+      </div>
+      <div className='waveArea'>
+        <div className='wave'></div>
+      </div>
+      <div className='formDataArea'>
+        <div className='formData'>
+          <div className='form'>FORM
+            <img src={Turtle} alt="" className='Turtle' /></div>
+          <div>
+            <p>store</p>
+            <div className='inputFirst' onClick={() => { setOpen(true); }}>
+              <input className='inputInner' type="text" placeholder=' placeholder text'
+                value={formInput.store} onChange={(e) => { setFormInput({ ...formInput, store: e.target.value }); }}
+              />
+
+              {open && <div className="inputOption" onClick={handlePropagation}>
+                {stores.filter(({ name }) => name.includes(formInput.store)).map(({ id, name }) => {
+                  return (
+                    <div className='item' key={`stores${id}`} onClick={() => { setFormInput({ ...formInput, store: name }); }}>{name}</div>
+                  );
+                })}
+              </div>}
+              <img src={Color} alt="" className='Color' />
+            </div>
+          </div>
+          <div>
+            <p> name</p>
+            <input className='input' placeholder='placeholder text' type="text" value={formInput.name} onChange={(e) => { setFormInput({ ...formInput, name: e.target.value }); }} required />
+          </div>
+          <div>
+            <p>phone</p>
+            <input className='input' placeholder='placeholder text' type="text" value={formInput.phone} onChange={(e) => { setFormInput({ ...formInput, phone: e.target.value }); }} required />
+          </div>
+          <div>
+            <p>Amount of consumption</p>
+            <input className='input' placeholder='placeholder text' type="text" value={formInput.amount} onChange={(e) => { setFormInput({ ...formInput, amount: e.target.value }); }} required />
+          </div>
+          <div>
+            <p> payment</p>
+            <div className='input'>digital payment<img src={Color} alt="" className='Color' />
+            </div>
+          </div>
+        </div>
+        <div className='Submit'>
+          <img src={Submit} alt='' />
+        </div>
+      </div>
+      <div className='areaThird'>
+        {awardText.map((allAward) => {
+          return (
+            <Awards allAward={allAward} key={`awardText${allAward.id}`} />
+          );
+        })}
+      </div>
+      <div className='areaFourth'>
+
+
+        <div className='areaFourthText'>
+          <p>13.32</p>
+          <p>who seeks after it and </p>
+          <p>wants to have it</p>
+          <img src={Vector4} alt="" className='Vector4' />
+          <img src={Vector5} alt="" className='Vector5' />
+        </div>
+        <div><img src={Lighthouse} alt='' className='Lighthouse' /></div>
+      </div>
+      <div className='areaFifth'>
+        <p>Neque porro quisquam
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>est qui dolorem!
+        </p>
+        <img src={Market} alt='' className='Market' />
+
+      </div>
+      <br />
+
     </div>
-  );
-}
+    <br />
+
+    {/* <img src='/png/14001_.png' alt='' className='' />
+    <img src='/png/1400_.png' alt='' className='' />
+
+    <br />
+    <img src='/png/文字輸入框說明.png' alt='' className='' />
+    <br />
+
+    <img src='/png/輸入資料說明.png' alt='' className='' /> */}
+
+
+  </>);
+};
 
 export default App;
---
+++ src/components/Awards.js
@@ -0,0 +1,20 @@
+import "../App.scss";
+
+const Awards = ({allAward}) => {
+    const { id, award, text, number, certificate
+    } = allAward
+    return (<>
+
+        <div className='present'>
+            <div className='certificateA'>
+                <img src={certificate} alt="" className="certificateImg"/>
+                <p className='certificateText'>{award}</p>
+            </div>
+            <div className='one'>{number}</div>
+            <p className='presentText'>{text}</p>
+        </div>
+
+    </>);
+};
+
+export default Awards;
